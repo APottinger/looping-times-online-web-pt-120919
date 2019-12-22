e77a28d11ecb07674559b67867fff745e2fc3f30
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-22 00:09:49 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,6 +1,6 @@
 cookies_in_the_cookie_jar = 50
 10.times do
-amount_of_cookies_in_the_cookie_jar = cookies_in_the_cookie_jar - 7
+amount_of_cookies_in_the_cookie_jar = cookies_in_the_cookie_jar - 2
   puts "There are #{amount_of_cookies_in_the_cookie_jar} left for us to eat"
 end
 
