6c944d052f2486167b03c399b579d38a970c8ef4
Allen Phillip Pottinger <allenpottinger@gmail.com>
2019-12-22 00:09:27 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,10 +1,7 @@
-
-
-7.times do
-  puts "Wingardium Leviosa"
-  wingardium_leviosa = wingardium_leviosa + epecto
+cookies_in_the_cookie_jar = 50
+10.times do
+amount_of_cookies_in_the_cookie_jar = cookies_in_the_cookie_jar - 7
+  puts "There are #{amount_of_cookies_in_the_cookie_jar} left for us to eat"
 end
-
-
 
 
